4ababe954e0543636ee14ec343627c71d9243167
zeineb-benmami <zeinebbenmami2016@gmail.com>
2024-10-20 00:56:35 +0200

    last2

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -4,49 +4,70 @@
     @include('layouts.partials/title-meta', ['title' => $title])
     @yield('css')
     @include('layouts.partials/head-css')
-    <link rel="stylesheet" href="../../scss/navbar.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 
+    <style>
+        /* Define the custom layout without using the .content-page */
+        .custom-layout {
+            width: 100%;
+            margin: 0;
+            padding: 0;
+            overflow-x: hidden; /* Prevent horizontal scroll */
+        }
+
+        .custom-content {
+            padding-left: 0 !important;
+            padding-right: 0 !important;
+            margin-left: 0 !important;
+            margin-right: 0 !important;
+            width: 100%;
+        }
+
+        .navbar-custom {
+            margin-bottom: 20px;
+        }
+
+        /* Ensure no extra padding/margin in the container */
+        .container-fluid {
+            padding-left: 0 !important;
+            padding-right: 0 !important;
+            margin-left: 0 !important;
+            margin-right: 0 !important;
+            width: 100%;
+        }
+    </style>
 </head>
 
 <body data-menu-color="light" data-sidebar="default" @yield('body') >
 
-<div id="app-layout">
-
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+<div class="custom-layout">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-custom">
         <div class="container-fluid">
-            <!-- Logo on the left -->
+            <!-- Logo -->
             <a class="navbar-brand ms-2 p-1" href="#">
-            <img src="{{ asset('images/Jardti.png') }}" alt="Logo" width="140" height="60">
+                <img src="{{ asset('images/Jardti.png') }}" alt="Logo" width="140" height="60">
             </a>
             <div class="d-flex gap-3">
-                <!-- Text in the center -->
+                <!-- Centered navbar links -->
                 <a href="/" class="nav-link">Home</a>
                 <a class="nav-link">Shop</a>
                 <a class="nav-link">Tasks</a>
                 <a class="nav-link">Forum</a>
                 <a class="nav-link">Trading</a>
                 <a class="nav-link" href="{{ route('eventsFront') }}">Events</a>
-                </div>
-            <!-- Button on the right -->
+            </div>
+            <!-- Button -->
             <button class="btn btn-success" type="button">Get Started</button>
         </div>
     </nav>
 
-
-
-    <div class="content-page">
-        <div class="content">
-            <div class="container-xxl">
-                @yield('content')
-            </div>
+    
+    <div class="custom-content">
+        <div class="container-fluid">
+            @yield('content')
         </div>
-
-        @include("layouts.partials/footer")
-
     </div>
 
+    @include("layouts.partials/footer")
 </div>
 
 @vite(['resources/js/app.js'])
